cb9d7fe6a237eeebcf632109d77084edcddc1ec6
wanggang <wanggang-bj@fang.com>
2017-03-07 17:27:27 +0800

    修改

--- CoreMvcApplication/src/CoreMvcApplication/Controllers/HelloWorldController.cs
+++ CoreMvcApplication/src/CoreMvcApplication/Controllers/HelloWorldController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,17 +14,22 @@ namespace CoreMvcApplication.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
-        {
-            return "This is my default action...";
-        }
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, id: {numTimes}");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
     }
 }
